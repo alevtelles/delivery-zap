e7cf329f336a1f585b9f6c2f63fbb100083a75da
Alexsander Valente <alevtelles@gmail.com>
2025-01-13 11:32:56 -0300

    Cardapior

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        home: "url('/assets/bg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
